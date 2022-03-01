d4b669395509cfd0ed35684257772409781d852e
Jonathan L <jonyliu99@gmail.com>
2022-03-01 02:32:39 -0500

    Add Test Level

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,20 +1,69 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class GameController : MonoBehaviour
 {
     public GameObject timerObj;
+    public GameObject inventoryObj;
+    public GameObject objectivesObj;
+
+    private TextMeshProUGUI inventoryTxt;
+    private TextMeshProUGUI objectiveTxt;
+    private Dictionary<string, bool> objectives = new Dictionary<string, bool>();
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         Timer timer = timerObj.GetComponent<Timer>();
+        inventoryTxt = inventoryObj.GetComponent<TextMeshProUGUI>();
+        objectiveTxt = objectivesObj.GetComponent<TextMeshProUGUI>();
         timer.StartTimer(100.0f);
+        SetObjectives(-1);
+        UpdateObjective("");
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void SetObjectives(int car) {
+        objectives.Add("tire", false);
+        objectives.Add("door", false);
+        objectives.Add("window", false);
+        string text = "<size=20>Objective</size>\n";
+        foreach(string key in objectives.Keys) {
+            text += key + "\n";
+        }
+        objectiveTxt.text = text;
+    }
+
+    public bool UpdateObjective(string part) {
+        if(objectives.ContainsKey(part) && !objectives[part]) {
+            objectives[part] = true;
+            string text = "<size=20>Objective</size>\n";
+            foreach(string key in objectives.Keys) {
+                if(objectives[key]) {
+                    text += "<s>" + key + "</s>\n";
+                }else {
+                    text += key + "\n";
+                }
+            }
+            objectiveTxt.text = text;
+            return true;
+        }
+
+        
+
+        return false;
+    }
+
+    public void UpdateInventory(Dictionary<string, int> inv) {
+        string text = "<size=20>Inventory</size>\n";
+        foreach(string key in inv.Keys) {
+            text += key + ": " + inv[key] + "\n";
+        }
+        inventoryTxt.text = text;
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,16 +6,19 @@ public class PlayerController : MonoBehaviour
 {
     public Camera camera;
     public float rayDistance;
-    public float distnace;
     public Dictionary<string, int> inventory {get; private set;}
+
+    private GameController gameController;
     // Start is called before the first frame update
     void Start()
     {
+        gameController = GameObject.Find("GameController").GetComponent<GameController>();
         inventory = new Dictionary<string, int>();
         // Dummie Data
         inventory.Add("metal", 1);
         inventory.Add("rubber", 2);
         inventory.Add("glass", 3);
+        gameController.UpdateInventory(inventory);
     }
 
     // Update is called once per frame
@@ -31,10 +34,31 @@ public class PlayerController : MonoBehaviour
         Ray ray = camera.ScreenPointToRay (Input.mousePosition);
 
         if (Physics.Raycast (ray, out hit, rayDistance)) {
-                if (hit.collider.tag == "Resource") {
-                    Debug.Log ("You hit a pickObject!");
-                    inventory[hit.gameObject.name] += 1;
+            if (hit.collider.tag == "Resource") {
+                Debug.Log ("You hit a pickObject!");
+                inventory[hit.collider.gameObject.name.ToLower()] += 1;
+                Destroy(hit.collider.gameObject);
+                gameController.UpdateInventory(inventory);
+            }else if(hit.collider.tag == "Part") {
+                Debug.Log("You picked up a part!");
+                if(!inventory.ContainsKey(hit.collider.gameObject.name)) {
+                    inventory.Add(hit.collider.gameObject.name.ToLower(), 0);
+                }
+                inventory[hit.collider.gameObject.name.ToLower()] += 1;
+                Destroy(hit.collider.gameObject);
+                gameController.UpdateInventory(inventory);
+
+            }else if(hit.collider.gameObject.name == "Broken Car") {
+                foreach(string item in inventory.Keys) {
+                    if(gameController.UpdateObjective(item)) {
+                        inventory[item]--;
+                        gameController.UpdateInventory(inventory);
+
+                        break;
+                    }
+                }
             }
+
         }
     }
 }
