f4f1c8287fe47179bd2072e53f0f02e5f0cbd14f
Jonathan L <jonyliu99@gmail.com>
2022-03-08 16:54:02 -0500

    Add Prefabs

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -8,9 +8,11 @@ public class GameController : MonoBehaviour
     public GameObject inventoryObj;
     public GameObject objectivesObj;
     public GameObject itemHighlightObj;
+    public GameObject tutorialObj;
 
     private TextMeshProUGUI inventoryTxt;
     private TextMeshProUGUI objectiveTxt;
+    private TextMeshProUGUI tutTxt;
     
     private TextMeshProUGUI itemTxt;
     private Dictionary<string, bool> objectives = new Dictionary<string, bool>();
@@ -21,21 +23,33 @@ public class GameController : MonoBehaviour
         inventoryTxt = inventoryObj.GetComponent<TextMeshProUGUI>();
         objectiveTxt = objectivesObj.GetComponent<TextMeshProUGUI>();
         itemTxt = itemHighlightObj.GetComponent<TextMeshProUGUI>();
-        timer.StartTimer(60.0f);
-        SetObjectives(-1);
+        tutTxt = itemHighlightObj.GetComponent<TextMeshProUGUI>();
+        timer.StartTimer(300.0f);
+        SetObjectives(0);
         UpdateObjective("");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 
     void SetObjectives(int car) {
-        objectives.Add("tire", false);
-        objectives.Add("door", false);
-        objectives.Add("window", false);
+        if(car == 0) {
+            objectives.Add("tire", false);
+            objectives.Add("door", false);
+            objectives.Add("window", false);
+        }else if(car == 1) {
+            objectives.Add("engine", false);
+            objectives.Add("seat", false);
+            objectives.Add("tire", false);            
+        }else if(car == 2) {
+            objectives.Add("engine", false);
+            objectives.Add("seat", false);
+            objectives.Add("tire", false);
+            objectives.Add("door", false);
+            objectives.Add("window", false);
+        }
         string text = "<size=20>Objective</size>\n";
         foreach(string key in objectives.Keys) {
             text += key + "\n";
