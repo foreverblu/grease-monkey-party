e283435a7a5b1950b85d8d450a4872fc03aed324
martinma425 <martinma425@gmail.com>
2022-03-08 17:37:52 -0500

    Update on level 2

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,98 +5,116 @@ using TMPro;
 using UnityEngine.SceneManagement;
 public class GameController : MonoBehaviour
 {
-    public GameObject timerObj;
-    public GameObject inventoryObj;
-    public GameObject objectivesObj;
-    public GameObject itemHighlightObj;
-    public GameObject tutorialObj;
+  public GameObject timerObj;
+  public GameObject inventoryObj;
+  public GameObject objectivesObj;
+  public GameObject itemHighlightObj;
+  public GameObject tutorialObj;
 
-    private TextMeshProUGUI inventoryTxt;
-    private TextMeshProUGUI objectiveTxt;
-    
-    private TextMeshProUGUI itemTxt;
-    private bool tutOver = false;
-    private Dictionary<string, bool> objectives = new Dictionary<string, bool>();
-    // Start is called before the first frame update
-    void Awake()
+  private TextMeshProUGUI inventoryTxt;
+  private TextMeshProUGUI objectiveTxt;
+
+  private TextMeshProUGUI itemTxt;
+  private bool tutOver = false;
+  private Dictionary<string, bool> objectives = new Dictionary<string, bool>();
+  // Start is called before the first frame update
+  void Awake()
+  {
+    inventoryTxt = inventoryObj.GetComponent<TextMeshProUGUI>();
+    objectiveTxt = objectivesObj.GetComponent<TextMeshProUGUI>();
+    itemTxt = itemHighlightObj.GetComponent<TextMeshProUGUI>();
+    SetObjectives(0);
+    UpdateObjective("");
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (tutorialObj != null && !tutOver && Input.GetMouseButtonDown(0))
     {
-        inventoryTxt = inventoryObj.GetComponent<TextMeshProUGUI>();
-        objectiveTxt = objectivesObj.GetComponent<TextMeshProUGUI>();
-        itemTxt = itemHighlightObj.GetComponent<TextMeshProUGUI>();
-        SetObjectives(0);
-        UpdateObjective("");
+      tutOver = true;
+      tutorialObj.SetActive(false);
     }
+  }
 
-    // Update is called once per frame
-    void Update()
+  void SetObjectives(int car)
+  {
+    Timer timer = timerObj.GetComponent<Timer>();
+    if (car == 0)
     {
-        if (tutorialObj != null && !tutOver && Input.GetMouseButtonDown(0)) {
-            tutOver = true;
-            tutorialObj.SetActive(false);
+      timer.StartTimer(60.0f);
+      objectives.Add("tire", false);
+      objectives.Add("door", false);
+      objectives.Add("window", false);
+    }
+    else if (car == 1)
+    {
+      timer.StartTimer(90.0f);
+      objectives.Add("engine", false);
+      objectives.Add("seat", false);
+      objectives.Add("tire", false);
+    }
+    else if (car == 2)
+    {
+      timer.StartTimer(300.0f);
+      objectives.Add("engine", false);
+      objectives.Add("seat", false);
+      objectives.Add("tire", false);
+      objectives.Add("door", false);
+      objectives.Add("window", false);
+    }
+    string text = "<size=20>Objective</size>\n";
+    foreach (string key in objectives.Keys)
+    {
+      text += key + "\n";
+    }
+    objectiveTxt.text = text;
+  }
+
+  public bool UpdateObjective(string part)
+  {
+    if (objectives.ContainsKey(part) && !objectives[part])
+    {
+      objectives[part] = true;
+      string text = "<size=20>Objective</size>\n";
+      foreach (string key in objectives.Keys)
+      {
+        if (objectives[key])
+        {
+          text += "<s>" + key + "</s>\n";
         }
-    }
-
-    void SetObjectives(int car) {
-        Timer timer = timerObj.GetComponent<Timer>();
-        if(car == 0) {
-            timer.StartTimer(60.0f);
-            objectives.Add("tire", false);
-            objectives.Add("door", false);
-            objectives.Add("window", false);
-        }else if(car == 1) {
-            timer.StartTimer(60.0f);
-            objectives.Add("engine", false);
-            objectives.Add("seat", false);
-            objectives.Add("tire", false);            
-        }else if(car == 2) {
-            timer.StartTimer(300.0f);
-            objectives.Add("engine", false);
-            objectives.Add("seat", false);
-            objectives.Add("tire", false);
-            objectives.Add("door", false);
-            objectives.Add("window", false);
+        else
+        {
+          text += key + "\n";
         }
-        string text = "<size=20>Objective</size>\n";
-        foreach(string key in objectives.Keys) {
-            text += key + "\n";
-        }
-        objectiveTxt.text = text;
+      }
+      objectiveTxt.text = text;
+      return true;
     }
 
-    public bool UpdateObjective(string part) {
-        if(objectives.ContainsKey(part) && !objectives[part]) {
-            objectives[part] = true;
-            string text = "<size=20>Objective</size>\n";
-            foreach(string key in objectives.Keys) {
-                if(objectives[key]) {
-                    text += "<s>" + key + "</s>\n";
-                }else {
-                    text += key + "\n";
-                }
-            }
-            objectiveTxt.text = text;
-            return true;
-        }
 
-        
 
-        return false;
+    return false;
+  }
+
+  public void UpdateInventory(Dictionary<string, int> inv)
+  {
+    string text = "<size=20>Inventory</size>\n";
+    foreach (string key in inv.Keys)
+    {
+      text += key + ": " + inv[key] + "\n";
     }
+    inventoryTxt.text = text;
+  }
 
-    public void UpdateInventory(Dictionary<string, int> inv) {
-        string text = "<size=20>Inventory</size>\n";
-        foreach(string key in inv.Keys) {
-            text += key + ": " + inv[key] + "\n";
-        }
-        inventoryTxt.text = text;
-    }
+  public void UpdateHighlight(string n)
+  {
+    itemTxt.text = n;
+  }
 
-    public void UpdateHighlight(string n) {
-        itemTxt.text = n;
-    }
-
-    public void GameOver() {
-        GameObject.Find("RaycastFPSController").GetComponent<PlayerController>().GameOver();
-        SceneManager.LoadScene (sceneName:"GameOver");
-    }
+  public void GameOver()
+  {
+    GameObject.Find("RaycastFPSController").GetComponent<PlayerController>().GameOver();
+    SceneManager.LoadScene(sceneName: "GameOver");
+  }
 }
